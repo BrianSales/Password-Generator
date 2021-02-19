2a20996b0ab5ffb1c02828ffc827645e36fe6eb6
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-02-19 11:29:14 -0600

    updated javascript

--- script.js
+++ script.js
@@ -1,83 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCaseArray= [
+
+    
+"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"    
+
+];
+
+console.log(upperCaseArray);
+
+var lowerCaseArray= [
+    "a","b","c","d","e","f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z"
+];
+
+var numbersArray= [
+    "1","2", "3", "4", "5", "6", "7", "8", "9", "0"
+];
+
+var specialCharactersArray= [
+    "!","@","#","$","%","^","&","*","(",")","_","+","-","{","}","<",">","?","/","."
+];
+
+var passwordNotes = [];
 
 // Write password to the #password input
 function writePassword() {
-  generatePassword();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
+};
 
 
-
+// User is propmted to give password length
 function generatePassword(){
     
-    var length= prompt("how long would you like your passward 8-128 characters");
+    var passwordlength = prompt("how long would you like your password 8-128 characters");
     console.log(length);
 
-    
+    if(passwordlength<8){
+        alert("Password must be at least 8 characters");
+    }
 
-    var uppercase= confirm("would you like uppercase letters");
-    var lowercase= confirm("would you like lowercase letters");
+    if(passwordlength>128){
+        aleart("Password must be less than 128 characters");
+    }
+};
+    
+// The prompts that ask questions after you hit the button
+    var upperCase= confirm("would you like uppercase letters");
+        if(upperCase){
+            passwordNotes.push(upperCaseArray);
+            console.log(passwordNotes);
+        }
+    var lowerCase= confirm("would you like lowercase letters");
+        if (lowerCase){
+            passwordNotes.push(lowerCaseArray);
+            console.log(passwordNotes);
+        }
+
     var numbers= confirm("would you like numbers");
+        if (numbers){
+            passwordNotes.push(numbersArray);
+            console.log(passwordNotes);
+        }
+
     var specialCharacters= confirm("would you like special-characters '!@#$'");
+        if (specialCharacters){
+            passwordNotes.push(specialCharactersArray);
+            console.log(passwordNotes);
+        }
 
+// Function writePassword(password){
 
-var uppercaseArray= [
-    
-    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
-
-]
-var lowerCaseArray= [
-    "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"
-]
-
-var numbersArray= [
-    1,2,3,4,5,6,7,8,9,0
-]
-
-var specialCharacters= [
-    "!","@","#","$","%","^","&","*","(",")","_","+"
-]
-
-if (numbers) 
-
-emptyArray= [
-
-    
-]
-}
-
-
-
-
-
-
-
-
-
-
-
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page;
