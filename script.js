6dc4af91ae4c2d1e37c20aeb64b8a05ac7e7b3f1
Brian Sales <mr.brian.mrs.kathleen@gmail.com>
2021-02-22 17:19:36 -0600

    added an screenshot and final project

--- script.js
+++ script.js
@@ -1,65 +1,9 @@
-// Assignment Code
-document.addEventListener("DOMContentLoaded",function(){
-    var generateBtn = document.getElementById("generate");
-    console.log(generateBtn)
-
-    generateBtn.addEventListener("click", function(){
-        console.log("i have been clicked")
-
-    var passwordlength = prompt("how long would you like your password 8-128 characters");
-    console.log(length);
-
-    if(passwordlength<8){
-        alert("Password must be at least 8 characters");
-    };
-
-    if(passwordlength>128){
-        aleart("Password must be less than 128 characters");
-    };
-
-    var upperCase= confirm("would you like uppercase letters");
-        if(upperCase){
-            passwordNotes.push(...upperCaseArray);
-            console.log(passwordNotes);
-        }
-    var lowerCase= confirm("would you like lowercase letters");
-        if (lowerCase){
-            passwordNotes.push(...lowerCaseArray);
-            console.log(passwordNotes);
-        }
-
-    var numbers= confirm("would you like numbers");
-        if (numbers){
-            passwordNotes.push(...numbersArray);
-            console.log(passwordNotes);
-        }
-
-    var specialCharacters= confirm("would you like special-characters '!@#$'");
-        if (specialCharacters){
-            passwordNotes.push(...specialCharactersArray);
-            console.log(passwordNotes);
-        }
-
-  var password = generatePassword(passwordlength);
-
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-
-    });
-
-
-
-
-
+//This is where I had to define the array sets
 var upperCaseArray= [
 
     "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"    
 
 ];
-
-
 
 var lowerCaseArray= [
     "a","b","c","d","e","f","g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v", "w", "x", "y", "z"
@@ -75,17 +19,67 @@ var specialCharactersArray= [
 
 var passwordNotes = [];
 
-// Write password to the #password input
+var passwordlength = [];
 
+//I choose to use queryselector syntax to help with page loadout and added an eventlistner so that the html loads first
 
+document.addEventListener("DOMContentLoaded",function(){
+    var generateBtn = document.querySelector("#generate");
+    console.log(generateBtn)
 
-
-
-// User is propmted to give password length
+    generateBtn.addEventListener("click", function(){
+        console.log("i have been clicked")
 
     
-// The prompts that ask questions after you hit the button
-    
+ //User is prompted with a series of ok or cancel prompts to help determine password length   
+        var passwordlength = prompt("How long would you like your password 8-128 characters");
+    console.log(length);
+
+    if(passwordlength<8){
+        alert("Password must from 8-128 characters");
+    } else if(passwordlength>128){
+            
+        alert("Password must be less than 128 characters");
+    };
+
+
+//User is prompted to slecet what values the password will contain used spread syntax method
+
+    var upperCase= confirm("Would you like uppercase letters?");
+        if(upperCase){
+            passwordNotes.push(...upperCaseArray);
+            console.log(passwordNotes);
+        }
+
+    var lowerCase= confirm("Would you like lowercase letters?");
+        if (lowerCase){
+            passwordNotes.push(...lowerCaseArray);
+            console.log(passwordNotes);
+        }
+
+    var numbers= confirm("Would you like numbers?");
+        if (numbers){
+            passwordNotes.push(...numbersArray);
+            console.log(passwordNotes);
+        }
+
+    var specialCharacters= confirm("Would you like special-characters. For example '!@#$'");
+        if (specialCharacters){
+            passwordNotes.push(...specialCharactersArray);
+            console.log(passwordNotes);
+        }
+
+  var password = generatePassword(passwordlength);
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+
+    });
+
+
+//function used to gather all variables and assign a random output
 function generatePassword(password){
     
     var passwordLoop = [];
@@ -94,12 +88,9 @@ function generatePassword(password){
     passwordLoop.push(passwordIndex)
     }
 
-
-    
-
     return passwordLoop.join("")
 
 };
 
-// Add event listener to generate button
+
 })
